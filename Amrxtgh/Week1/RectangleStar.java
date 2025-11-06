a4939ee0953c22ff881c6063a6f4f8b21771a75b
amrxtgh <ghimireamrit619@gmail.com>
2025-11-06 16:10:26 +0545

    Complete all week1 and week3

---
+++ Amrxtgh/Week1/RectangleStar.java
@@ -0,0 +1,9 @@
+public class RectangleStar {
+  public static void main(String[] args) {
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+  }
+}
---
+++ Amrxtgh/Week1/Wap.java
@@ -0,0 +1,5 @@
+public class Wap {
+  public static void main(String[] args) {
+    System.out.println("A well-formed Java\nprogram has a main\nmethod with { and }\nbraces.\n\nA System.out.println\nstatement has ( and )\nand usually a String that\nstarts and ends with a \"\ncharacter.\n(But we type \\\" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \\\"?\n\nOne is what we see whenwe're typing\nour program. The other is what\nappears on the \"console.\"");
+  }
+}
